42b96bcc9ebb9f0b0c6819b2e0f71e4c9362d67f
Tilman Miraß <mail@tlmn.io>
2020-11-23 15:47:33 +0100

    remove lib

--- src/components/lib/lib.jsx
+++
@@ -1,8 +0,0 @@
-import emojiRegex from "emoji-regex";
-
-export const formatEmojis = (text = "") => {
-  return text.replace(
-    emojiRegex(),
-    (m) => `<span class="not-italic" role="img">${m}</span>`
-  );
-};
